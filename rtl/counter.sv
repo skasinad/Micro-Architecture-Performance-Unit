77c6165a693df6fa157310d9af5cb6d53bbd198f
Satvik Kasinadhuni <skasinad@gmail.com>
2026-03-16 13:48:03 -0400

    counter module

---
+++ rtl/counter.sv
@@ -0,0 +1,18 @@
+module counter#(
+    parameter int DEPTH = 16 //16 bits long 
+)(
+    input logic clk, 
+    input logic rst, 
+    input logic enable, 
+    input logic sigevent, 
+    output logic[DEPTH-1:0] crntcount
+);
+    always_ff @(posedge clk) begin 
+        if(rst) begin
+            crntcount <= '0; 
+        end else if (enable && sigevent) begin
+            crntcount <= crntcount + 1; 
+        end 
+    end 
+
+endmodule 
